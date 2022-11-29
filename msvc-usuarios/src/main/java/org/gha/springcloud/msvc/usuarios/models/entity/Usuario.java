f19d77bd08262565ec6ba60d56f3f7f24fed7668
giovanny hernandez <giovanni104@hotmail.com>
2022-11-28 19:03:02 -0500

    avances del proyecto

--- msvc-usuarios/src/main/java/org/gha/springcloud/msvc/usuarios/models/entity/Usuario.java
+++ msvc-usuarios/src/main/java/org/gha/springcloud/msvc/usuarios/models/entity/Usuario.java
@@ -1,11 +1,7 @@
 package org.gha.springcloud.msvc.usuarios.models.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Table(name="usuarios")
@@ -15,11 +11,15 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nombre;
 
+    @NotEmpty
+    @Email
     @Column(unique = true)
     private String email;
 
+    @NotBlank
     private String password;
 
     public Long getId() {
---
+++ msvc-usuarios/src/main/java/org/gha/springcloud/msvc/usuarios/services/UsuarioServiceImpl.java
@@ -0,0 +1,52 @@
+package org.gha.springcloud.msvc.usuarios.services;
+
+import org.gha.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.gha.springcloud.msvc.usuarios.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Usuario> listar() {
+        return (List<Usuario>) repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Usuario> porId(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Usuario guardar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+    
+    @Override
+    public Optional<Usuario> porEmail(String email) {
+        return repository.porEmail(email);
+    }
+
+    @Override
+    public boolean existePorEmail(String email) {
+        return repository.existsByEmail(email);
+    }
+    
+}
